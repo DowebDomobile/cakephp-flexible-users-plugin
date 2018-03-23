b45c29560fdfe136f321ba41323da7b81fec1bbf
Ivan Vorsin <ivanboroda@gmail.com>
2018-03-23 16:50:00 +0700

    Use view trait in login action.

---
+++ src/Action/LoginAction.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @copyright     Copyright (c) DowebDomobile (http://dowebdomobile.ru)
+ */
+
+namespace Dwdm\Users\Action;
+
+use Crud\Traits\ViewTrait;
+
+/**
+ * Class LoginAction
+ * @package Dwdm\Users\Action
+ */
+class LoginAction extends \CrudUsers\Action\LoginAction
+{
+    use ViewTrait;
+}
